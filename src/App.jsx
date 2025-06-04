23f2f8e3d9765056905e4622ad9fa396843cfd1d
David-BarraganP <david25barragan@gmail.com>
2025-06-03 21:33:15 -0500

    Primer commit del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,21 @@
-import React, { useState, useEffect } from "react";
-import { Navigation } from "./components/navigation";
-import { Header } from "./components/header";
-import { Features } from "./components/features";
-import { About } from "./components/about";
-import { Services } from "./components/services";
-import { Gallery } from "./components/gallery";
-import { Testimonials } from "./components/testimonials";
-import { Team } from "./components/Team";
-import { Contact } from "./components/contact";
-import JsonData from "./data/data.json";
-import SmoothScroll from "smooth-scroll";
-import "./App.css";
 
-export const scroll = new SmoothScroll('a[href*="#"]', {
-  speed: 1000,
-  speedAsDuration: true,
-});
 
-const App = () => {
-  const [landingPageData, setLandingPageData] = useState({});
-  useEffect(() => {
-    setLandingPageData(JsonData);
-  }, []);
 
+import React from "react";
+import { QRCodeCanvas } from "qrcode.react";
+
+const QrPage = () => {
   return (
-    <div>
-      <Navigation />
-      <Header data={landingPageData.Header} />
-      <Features data={landingPageData.Features} />
-      <About data={landingPageData.About} />
-      <Services data={landingPageData.Services} />
-      <Gallery data={landingPageData.Gallery} />
-      <Testimonials data={landingPageData.Testimonials} />
-      <Team data={landingPageData.Team} />
-      <Contact data={landingPageData.Contact} />
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h2>Escanea el QR </h2>
+      <QRCodeCanvas
+        value="http://localhost:3000"
+        size={200}
+        fgColor="#000000"
+      />
     </div>
   );
 };
 
-export default App;
+export default QrPage;
+
